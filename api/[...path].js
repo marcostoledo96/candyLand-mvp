417cf87b0044c68a818f00592371eeb08d9c8ccc
Marcos Toledo <164115802+marcostoledo96@users.noreply.github.com>
2025-11-09 19:55:46 -0300

    chore(api): remove .js serverless handlers, keep .cjs to ensure CommonJS on Vercel

--- api/[...path].js
+++
@@ -1,5 +0,0 @@
-// Catch-all serverless function for all /api/* routes using Express app
-const serverless = require('serverless-http');
-const app = require('../backend/app');
-
-module.exports = serverless(app);
--- api/index.js
+++
@@ -1,5 +0,0 @@
-// Root /api function (optional convenience)
-const serverless = require('serverless-http');
-const app = require('../backend/app');
-
-module.exports = serverless(app);
